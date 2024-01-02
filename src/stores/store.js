80f55e844b7353f0d82ece51ea8ab5c3dfaed60c
Bidyut Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2024-01-02 20:06:00 +0600

    Update store.js

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,21 +1,4 @@
 
-import CounterSlice from "../reduxSlices/CounterSlice";
-import { configureStore } from "@reduxjs/toolkit";
-
-
-const store=configureStore({
-   reducer:{
-    counter:CounterSlice
-   }
-})
-
-
-
-
-
-
-
-export default store;
 
 
 
